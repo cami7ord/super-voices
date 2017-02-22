2aad2510b6640539c92c18287d6f0c63d7f1b81c
cami7ord <cami7ord@gmail.com>
2017-02-22 14:04:47 -0500

    Creating contest working properly, still needs to separate the Contests by Admin

--- super-voices/app/admin/contest.rb
+++ super-voices/app/admin/contest.rb
@@ -1,5 +1,4 @@
 ActiveAdmin.register Contest do
-
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -12,6 +11,6 @@ ActiveAdmin.register Contest do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
+    permit_params :name, :picture, :url, :start, :end, :payment, :script, :guidelines
 
 end
