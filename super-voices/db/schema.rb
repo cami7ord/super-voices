4feb4133de646a9caf743800935b50bb57ec24e2
cami7ord <cami7ord@gmail.com>
2017-02-21 23:12:24 -0500

    Migrated the database to reflect the addition of the CONTEST model

--- super-voices/db/schema.rb
+++ super-voices/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222031610) do
+ActiveRecord::Schema.define(version: 20170222040450) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -41,6 +41,19 @@ ActiveRecord::Schema.define(version: 20170222031610) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "contests", force: :cascade do |t|
+    t.string   "name"
+    t.string   "picture"
+    t.string   "url"
+    t.date     "start"
+    t.date     "end"
+    t.decimal  "payment"
+    t.text     "script"
+    t.text     "guidelines"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
